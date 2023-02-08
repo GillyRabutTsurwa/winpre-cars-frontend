e84c35ea37d1a5784a7e5e45c4d610b6d77b73d1
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-02-07 20:58:41 -0500

    Making Image Slideshow in Banner Component - it is important to note that i am NOT using the SanityImage helper component - i need to access the url outside that component with how i made my slideshow - i found the solution thanks to this link: https://sanity.nuxtjs.org/helpers/images/ - the @sanity/image-url plugin does all of the work for me 	- all i need to do is to use the $urlFor() 	- which takes the whole image object as the parametre 	- note this is very different from the SanityImage component which does not except the entire object 	- instead it (SanityImage) accepts the URL value in a special attribute called asset-id 	- with the plugin, i don't need to worry about that. just include the whole object, and the plugin does the rest

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,8 +1,33 @@
 <template>
-  <figure></figure>
+  <figure :style="{backgroundImage: `url(${$urlFor(currentImage)})`}"></figure>
 </template>
 
 <script setup>
+// <figure :style="{backgroundImage: `url(${$urlFor(randomImage)})`}"></figure>
+const currentIndex = ref(0);
+
+const query = `*[_type=="bannerImages"]`;
+const { data, error } = await useSanityQuery(query);
+const images = data.value[0].images;
+
+const nextImage = () => {
+  currentIndex.value += 1;
+  if (currentIndex.value >= images.length) currentIndex.value = 0;
+};
+
+const currentImage = computed(() => {
+  return images[currentIndex.value];
+});
+
+//
+
+const startImageSlideShow = () => {
+  setInterval(nextImage, 5000);
+};
+
+onMounted(() => {
+  startImageSlideShow();
+});
 </script>
 
 <style lang="scss" scoped>
@@ -10,10 +35,54 @@
 figure {
   width: 100%;
   height: calc(100vh - 15rem);
-  background-image: url(../assets/img/banner-8.jpg);
+  // background-image: url(../assets/img/banner-8.jpg);
   background-size: cover;
   background-repeat: no-repeat;
   background-position-y: -15rem;
   background-attachment: fixed;
+  opacity: 1;
+
+  // animation-name: fade;
+  // animation-timing-function: ease-in;
+  // animation-iteration-count: infinite;
+  // animation-duration: 7s;
+  // // animation-delay: 4s;
+  // animation-direction: alternate;
+  -webkit-transition: background-image 1.5s linear;
+  -moz-transition: background-image 1.5s linear;
+  -o-transition: background-image 1.5s linear;
+  -ms-transition: background-image 1.5s linear;
+  transition: background-image 1.5s linear;
+}
+
+@-webkit-keyframes fade {
+  0% {
+    opacity: 0;
+  }
+  45% {
+    opacity: 0;
+  }
+  55% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+/* Standard syntax */
+@keyframes fade {
+  0% {
+    opacity: 0;
+  }
+  45% {
+    opacity: 0;
+  }
+  55% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
