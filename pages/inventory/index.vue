9ad8376e58fc070dda2955f7b5315643a486188c
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-03 20:06:21 -0400

    Issue Not Populating

--- pages/inventory/index.vue
+++ pages/inventory/index.vue
@@ -3,8 +3,6 @@ const route = useRoute();
 console.log(route);
 console.log(route.query);
 
-// @todo: Make Helper Function or Composable For Below Function
-// this function is better placed someplace else where it is globally accessible. ie, helper or composable
 const capitalise = (str) => `${str.charAt(0).toUpperCase()}${str.substring(1)}`;
 
 let queryProperty;
