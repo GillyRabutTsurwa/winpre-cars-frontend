467d8b7619b9940f49b9fcd990bfa6a1fda343be
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-05-09 21:42:31 -0400

    Don't Know What's Going On Avec Ceci

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container" ref="container">
-    <!-- <Header ref="header" /> -->
+    <Header ref="header" />
     <slot />
-    <!-- <Footer /> -->
+    <Footer />
   </div>
 </template>
 
